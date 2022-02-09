b087d85a35acdc13ac81ab900e7bb70d831954a3
ololosh-tralevich <karpeko76@gmail.com>
2022-02-09 19:47:43 +0200

    done task 4

--- src/components/fourthTask/fourthTask.jsx
+++ src/components/fourthTask/fourthTask.jsx
@@ -1,15 +1,29 @@
-import transactions from './transactions.json';
+import transactionsData from './transactions.json';
 import styles from './fourthTask.module.css';
 
 const FourthTask = () => {
+  let transactions = transactionsData.map(item => {
+    return (
+      <tr className={styles.tableColumn}>
+        <td className={styles.tableData}>{item.type}</td>
+        <td className={styles.tableData}>{item.amount}</td>
+        <td className={styles.tableData}>{item.currency}</td>
+      </tr>
+    );
+  });
   return (
-    <table className={styles.transactionTable}>
-      <thead>
-        <th>Type</th>
-        <th>Amount</th>
-        <th>Currency</th>
-      </thead>
-    </table>
+    <div className={styles.mainSection}>
+      <table className={styles.transactionsTable}>
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Currency</th>
+          </tr>
+        </thead>
+        <tbody>{transactions}</tbody>
+      </table>
+    </div>
   );
 };
 
