0bd45e715ca91b3a75f8d9573cc4cd8235c1e33c
ololosh-tralevich <karpeko76@gmail.com>
2022-02-09 18:21:36 +0200

    done second task

--- src/components/first_task/FirstTask.jsx
+++ src/components/first_task/FirstTask.jsx
@@ -1,25 +1,25 @@
-import './first_task.css';
+import styles from './firstTask.module.css';
 import userData from './user.json';
 
 // console.log(userData)
 
 const FirstTask = () => {
   return (
-    <div className="main-section">
+    <div className={styles.mainSection}>
       <img src={userData.avatar} width="100px" alt='User Avatar'></img>
       <h2>{userData.username}</h2>
       <h3>@{userData.tag}</h3>
       <h3>{userData.location}</h3>
-      <ul className="main-section-list">
-        <li className="list-item">
+      <ul className={styles.mainSectionList}>
+        <li className={styles.listItem}>
           <p>Followers</p>
           <p>{userData.stats.followers}</p>
         </li>
-        <li className="list-item">
+        <li className={styles.listItem}>
           <p>Views</p>
           <p>{userData.stats.views}</p>
         </li>
-        <li className="list-item">
+        <li className={styles.listItem}>
           <p>Likes</p>
           <p>{userData.stats.likes}</p>
         </li>
--- src/components/second_task/secondTask.jsx
+++ src/components/second_task/secondTask.jsx
@@ -1,27 +1,31 @@
-import './secondTask.css';
+import styles from './secondTask.module.css';
+import statsData from './data.json';
+
+let randomColor = '';
 
 const secondTask = () => {
+  let partOfCode = statsData.map(item => {
+    getRandomColor();
+
+    return (
+      <li key={item.id} className={styles.listItem} style={{ backgroundColor: randomColor }}>
+        <p className={styles.itemText}>{item.label}</p>
+        <p className={styles.itemText}>{item.percentage}%</p>
+      </li>
+    );
+  });
+
   return (
-<div className='main-section'>
-    <h2>UPLOAD STATS</h2>
-    <ul><li>
-        <p></p>
-        <p></p>
-    </li>
-    <li>
-        <p></p>
-        <p></p>
-    </li>
-    <li>
-        <p></p>
-        <p></p>
-    </li>
-    <li>
-        <p></p>
-        <p></p>
-    </li></ul>
-</div>
+    <div className={styles.mainSection}>
+      <h2>UPLOAD STATS</h2>
+      <ul className={styles.mainSectionList}>{partOfCode}</ul>
+    </div>
   );
 };
 
 export default secondTask;
+
+function getRandomColor() {
+  randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return randomColor;
+}
