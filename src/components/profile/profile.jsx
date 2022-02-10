296cc5604620977cc19563deb30de096955c919b
ololosh-tralevich <karpeko76@gmail.com>
2022-02-10 15:40:49 +0200

    upd

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -39,7 +39,7 @@ Profile.propTypes = {
     followers: PropTypes.number.isRequired,
     views: PropTypes.number.isRequired,
     likes: PropTypes.number.isRequired,
-  }),
+  }).isRequired,
 };
 
 export default Profile;
